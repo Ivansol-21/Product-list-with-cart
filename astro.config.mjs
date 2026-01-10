c1e7668ca1caeaed77ff41ea770420056e2c9bf2
Ivan solano <ivanheerr13@gmail.com>
2026-01-09 20:20:33 -0500

    Deploy prueba 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ivansol-21.github.io",
+  base: "/Product-list-with-cart/",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
